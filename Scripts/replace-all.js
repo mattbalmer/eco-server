422d2ee1f5cd180042275d4dd15e8079551e928d
mattbalmer <signup@mattbalmer.com>
2021-02-03 18:02:01 -0800

    .

--- Scripts/replace-all.js
+++ Scripts/replace-all.js
@@ -20,12 +20,12 @@ replaceRecursive(path.resolve(__dirname, '..', 'Mods'),
 
       .replace(/MaturityAgeDays = 0.8f/g, "MaturityAgeDays = 0.65f")
       .replace(/MaturityAgeDays = 1f/g, "MaturityAgeDays = 0.8f")
-      .replace(/MaturityAgeDays = 4.5f;/g, "MaturityAgeDays = 2.5f;")
-      .replace(/MaturityAgeDays = 5;/g, "MaturityAgeDays = 3;")
-      .replace(/MaturityAgeDays = 5.5f/g, "MaturityAgeDays = 3.5")
-      .replace(/MaturityAgeDays = 6/g, "MaturityAgeDays = 4f")
-      .replace(/MaturityAgeDays = 7/g, "MaturityAgeDays = 4.5f")
-      .replace(/MaturityAgeDays = 30/g, "MaturityAgeDays = 10")
+      .replace(/MaturityAgeDays = 4.5f/g, "MaturityAgeDays = 2.5f")
+      .replace(/MaturityAgeDays = 5;/g, "MaturityAgeDays = 3f;")
+      .replace(/MaturityAgeDays = 5.5f/g, "MaturityAgeDays = 3.5f")
+      .replace(/MaturityAgeDays = 6;/g, "MaturityAgeDays = 4f;")
+      .replace(/MaturityAgeDays = 7;/g, "MaturityAgeDays = 4.5f;")
+      .replace(/MaturityAgeDays = 30;/g, "MaturityAgeDays = 10f;")
     ;
   }
 );
