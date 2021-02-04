d2cfe30e5d1b0c480eff83518f81254f74e1b7ec
mattbalmer <signup@mattbalmer.com>
2021-02-03 17:42:45 -0800

    .

--- Scripts/replace-all.js
+++ Scripts/replace-all.js
@@ -2,23 +2,32 @@ const path = require('path');
 const fs = require('fs');
 const { replaceRecursive } = require('./utils/replace-recursive');
 
-replaceRecursive(path.resolve(__dirname, '..', 'Mods'), (content, filename) => {
-  console.log(`Replacing ${filename}`);
-  return content
-    .replace(/\[MaxStackSize\(20\)\]/g, "[MaxStackSize(40)]")
-    .replace(/\[MaxStackSize\(15\)\]/g, "[MaxStackSize(60)]")
-    .replace(/\[MaxStackSize\(10\)\]/g, "[MaxStackSize(40)]")
+replaceRecursive(path.resolve(__dirname, '..', 'Mods'),
+  {
+    ignore: [
+      '.git',
+      'Scripts',
+      'WebClient',
+    ]
+  },
+  (content, filename) => {
+    console.log(`Replacing ${filename}`);
+    return content
+      .replace(/\[MaxStackSize\(20\)\]/g, "[MaxStackSize(40)]")
+      .replace(/\[MaxStackSize\(15\)\]/g, "[MaxStackSize(60)]")
+      .replace(/\[MaxStackSize\(10\)\]/g, "[MaxStackSize(40)]")
 
-    .replace(/MaturityAgeDays = 0.8f/g, "MaturityAgeDays = 0.65f")
-    .replace(/MaturityAgeDays = 1f/g, "MaturityAgeDays = 0.8f")
-    .replace(/MaturityAgeDays = 4.5f;/g, "MaturityAgeDays = 2.5f;")
-    .replace(/MaturityAgeDays = 5;/g, "MaturityAgeDays = 3;")
-    .replace(/MaturityAgeDays = 5.5f/g, "MaturityAgeDays = 3.5")
-    .replace(/MaturityAgeDays = 6/g, "MaturityAgeDays = 4f")
-    .replace(/MaturityAgeDays = 7/g, "MaturityAgeDays = 4.5f")
-    .replace(/MaturityAgeDays = 30/g, "MaturityAgeDays = 10")
-  ;
-});
+      .replace(/MaturityAgeDays = 0.8f/g, "MaturityAgeDays = 0.65f")
+      .replace(/MaturityAgeDays = 1f/g, "MaturityAgeDays = 0.8f")
+      .replace(/MaturityAgeDays = 4.5f;/g, "MaturityAgeDays = 2.5f;")
+      .replace(/MaturityAgeDays = 5;/g, "MaturityAgeDays = 3;")
+      .replace(/MaturityAgeDays = 5.5f/g, "MaturityAgeDays = 3.5")
+      .replace(/MaturityAgeDays = 6/g, "MaturityAgeDays = 4f")
+      .replace(/MaturityAgeDays = 7/g, "MaturityAgeDays = 4.5f")
+      .replace(/MaturityAgeDays = 30/g, "MaturityAgeDays = 10")
+    ;
+  }
+);
 
 /*
 Manual Changes:
--- Scripts/utils/replace-recursive.js
+++ Scripts/utils/replace-recursive.js
@@ -7,7 +7,8 @@ const util = require('util');
 const readdir = util.promisify(fs.readdir);
 
 // todo: fix this damn file
-async function getFiles(path = "./") {
+async function getFiles(dirname = './', options = {}) {
+  const ignore = options.ignore || [];
   const entries = await readdir(path, { withFileTypes: true });
 
   // Get files within the current directory and add a path key to the file objects
@@ -18,40 +19,54 @@ async function getFiles(path = "./") {
   // Get folders within the current directory
   const folders = entries.filter(folder => folder.isDirectory());
 
-  for (const folder of folders)
+  for (const folder of folders) {
       /*
         Add the found files within the subdirectory to the files array by calling the
         current function itself
       */
+    if (!options.ignore.includes(folder.name)) {
       files.push(...await getFiles(`${path}${folder.name}/`));
+    }
+  }
 
   return files;
 }
 
-async function readFiles(dirname, onFileContent, onError) {
-  const files = await getFiles(dirname);
-  fs.readdir(dirname, function(err, filenames) {
-    if (err) {
-      onError(err);
-      return;
-    }
-    filenames.forEach(function(filename) {
-      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
-        if (err) {
-          onError(err);
-          return;
-        }
-        onFileContent(filename, content);
-      });
-    });
+async function readFiles(dirname, options, onFileContent, onError) {
+  const files = await getFiles(dirname, options);
+  console.log('files', files);
+  files.forEach(file => {
+    const { name, path } = file;
+
   });
+  // fs.readdir(dirname, function(err, filenames) {
+  //   if (err) {
+  //     onError(err);
+  //     return;
+  //   }
+  //   filenames.forEach(function(filename) {
+  //     fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+  //       if (err) {
+  //         onError(err);
+  //         return;
+  //       }
+  //       onFileContent(filename, content);
+  //     });
+  //   });
+  // });
 }
 
-function replaceRecursive(dirname, getContent) {
-  readFiles('./',
+function replaceRecursive(dirname, getContent, onSuccess) {
+  readFiles(dirname,
     function(filename, content) {
       const newcontent = getContent(content, filename);
-      fs.writeFile(filename, newcontent, { encoding: 'utf8' });
+      fs.writeFile(filename, newcontent, { encoding: 'utf8' }, (err) => {
+        if (err) {
+          throw err;
+        } else if(onSuccess) {
+          onSuccess();
+        }
+      });
     },
     function(err) {
       throw err;
@@ -63,17 +78,3 @@ module.exports = {
   readFiles,
   replaceRecursive,
 };
-
-readFiles('./',
-  function(filename, content) {
-    console.log(filename);
-    // var newcontent=content.replace(/\n"\nauthor/,"\nauthor");
-    const newcontent = content.replace(/title:\s*"([^"]*)\nauthor/,"title: \"$1\"\nauthor");
-    fs.writeFile(filename, newcontent, { encoding: 'utf8' }, function() {
-        console.log('OK '+filename);
-    });
-  },
-  function(err) {
-    throw err;
-  }
-);
